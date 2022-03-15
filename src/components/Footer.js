4a9c468fe2a1b951234fc18b101311e49cd85c5f
Waldo Smuts <wb.smuts@gmail.com>
2022-03-15 13:08:27 +0200

    removed unused variable

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,5 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-
 export default function Footer() {
-    const date = new Date
+    const date = new Date()
     return (
         <footer className="fixed bottom-0 left-0 bg-zinc-900 dark:bg-white w-full py-px px-4 flex items-center">
             <span className="mt-1 tracking-wider font-bold uppercase text-xs text-zinc-300 dark:text-zinc-800">Waldo Smuts © {date.getFullYear()}</span>
